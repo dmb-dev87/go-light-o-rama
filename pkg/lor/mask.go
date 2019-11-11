067abdaedfa4a88b778bef4a106a5fbe705f224f
dmb-dev87 <dmbdev800@gmail.com>
2019-11-11 17:07:33 -0600

    add comments to Mask

--- pkg/lor/mask.go
+++ pkg/lor/mask.go
@@ -7,11 +7,13 @@ import (
 
 var errBadLength = errors.New("bad bitLength")
 
+// Mask represents a variable length bit mask.
 type Mask struct {
 	offset byte
 	b      []byte
 }
 
+// Set sets the bit value at the specified index to the specified value.
 func (m *Mask) Set(ch Channel, val bool) {
 	var bitIndex = byte(ch) % 8
 	var byteIndex = int(math.Floor(float64(ch) / 8))
@@ -23,6 +25,7 @@ func (m *Mask) Set(ch Channel, val bool) {
 	}
 }
 
+// SetAll sets all bit values within the mask length to the specified value.
 func (m *Mask) SetAll(val bool) {
 	for i := 0; i < len(m.b); i++ {
 		if val {
@@ -33,6 +36,7 @@ func (m *Mask) SetAll(val bool) {
 	}
 }
 
+// NewMask returns a new mask sized for the specified bit length.
 func NewMask(bitLength byte) (*Mask, error) {
 	var offset, ok = magicOffsetTable[bitLength]
 	if !ok {
